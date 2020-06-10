cd6f9e8cc1c0d866bd0085ef43ae2eda36f841b4
Matheus Mello <matheusamellom95@gmail.com>
2020-06-10 16:10:27 -0300

    Versão Inicial

---
+++ buscar-curso.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'vendor/autoload.php';
+
+Teste::metodo();
+exit();
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$cliente = new Client(['base_uri' =>'https://alura.com.br/']);
+$crawler = new Crawler();
+
+$buscador = new Buscador($cliente, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+
+foreach ($cursos as $curso){
+    echo $curso . PHP_EOL;
+}
